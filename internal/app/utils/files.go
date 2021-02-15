79d819025b169f502742638e67182f2dcd4e3361
Denys Polishchuk <den.polischuk19@gmail.com>
2021-02-15 19:52:09 +0100

    chore(cli): move func to more propriate file

--- internal/app/utils/files.go
+++ internal/app/utils/files.go
@@ -2,26 +2,10 @@ package utils
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 	"os"
-	"path/filepath"
 	"strings"
 )
-
-// PromptPathToResource - Asks user path to zsh or uses default one if user input is empty
-func PromptPathToResource(promptStr string, def string) string {
-	path := def
-	var userInput string
-	fmt.Printf("%s [%s]: ", promptStr, path)
-	userInput = ""
-	fmt.Scanln(&userInput)
-	if userInput != "" {
-		path = userInput
-	}
-
-	return filepath.Clean(path)
-}
 
 // FileContains searches string (str) in file (f). Returns true if str was found in f.
 func FileContains(f io.Reader, str string) (bool, error) {
